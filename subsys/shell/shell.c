d2ad5d054439e86b8fbe5bed6e48b9f0dbd1330f
Josh Gao <josh@jmgao.dev>
2020-11-10 21:18:10 -0800

    Increase priority of logging and shell threads.

--- subsys/shell/shell.c
+++ subsys/shell/shell.c
@@ -1275,7 +1275,7 @@ int shell_init(const struct shell *shell, const void *transport_config,
 			      shell->stack, CONFIG_SHELL_STACK_SIZE,
 			      shell_thread, (void *)shell, (void *)log_backend,
 			      UINT_TO_POINTER(init_log_level),
-			      K_LOWEST_APPLICATION_THREAD_PRIO, 0, K_NO_WAIT);
+			      K_LOWEST_APPLICATION_THREAD_PRIO - 1, 0, K_NO_WAIT);
 
 	shell->ctx->tid = tid;
 	k_thread_name_set(tid, shell->thread_name);
